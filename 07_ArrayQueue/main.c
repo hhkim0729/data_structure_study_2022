6ed39fe5f5317887f5c4992901b6fa736b5314a2
hhkim0729 <heehkim@student.42seoul.kr>
2022-02-09 18:04:18 +0900

    Add array queue, linked deque

---
+++ 07_ArrayQueue/main.c
@@ -0,0 +1,13 @@
+#include "arrayqueue.h"
+
+void	test(void)
+{
+
+}
+
+int	main(void)
+{
+	test();
+	// system("leaks arrayqueue");
+	return (0);
+}
---
+++ 08_LinkedDeque/main.c
@@ -0,0 +1,13 @@
+#include "linkeddeque.h"
+
+void	test(void)
+{
+
+}
+
+int	main(void)
+{
+	test();
+	// system("leaks linkeddeque");
+	return (0);
+}
